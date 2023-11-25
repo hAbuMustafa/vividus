af2538b5e5bbc28ceeea9f27540a29269734b10d
hAbuMustafa <hosam5553@gmail.com>
2023-11-25 18:12:32 +0200

    added use example

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,3 +1,47 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Abbr } from '@vividus/svelte';
+</script>
+
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint magni, excepturi corrupti
+modi velit eveniet possimus officia nam voluptate consectetur suscipit fugiat dolores
+placeat rem saepe nesciunt ipsum quia facilis corporis repudiandae nostrum eaque, fuga
+inventore. Reiciendis accusamus similique suscipit eligendi consectetur mollitia tenetur
+earum a, magni cum quae corporis dolorum est recusandae veritatis pariatur illo incidunt
+quo quas voluptate. Hic dicta consectetur perferendis saepe quas vel labore vero
+accusantium, soluta repellendus corrupti provident quisquam ullam praesentium maxime
+maiores deserunt cum dolorum blanditiis adipisci, laborum amet molestiae. Id labore quod
+officia necessitatibus at quisquam voluptate inventore optio tempore a. Velit aspernatur
+perferendis tenetur laboriosam ratione doloremque quisquam eos rerum suscipit minima
+voluptatum reiciendis illo, libero voluptatem placeat nam est eaque id quidem dolorem
+possimus. Et rem earum libero a ea eaque officia numquam eum perferendis! Voluptatem
+doloribus consectetur perferendis sit rem, quasi omnis tenetur, maxime suscipit maiores
+minima nesciunt! Distinctio molestiae nostrum nulla totam deserunt animi perferendis
+doloremque facere id laudantium. Earum, aspernatur enim? Dolore illum dolor, repellat
+architecto veniam, velit quis blanditiis facere ipsum similique enim libero eveniet nemo
+labore? Delectus vel quos dolore asperiores sed magni dolores quidem earum ipsa
+consectetur velit adipisci eveniet quo cupiditate, iste eum aut nihil. Maxime accusamus
+excepturi facilis fugit accusantium maiores odit aspernatur, doloribus quo, iure ratione
+laudantium vel itaque earum quos optio architecto nemo dolor perferendis eum ducimus
+veritatis aut sunt eveniet? Rem, nostrum inventore molestias numquam minima quos assumenda
+ratione iste praesentium? Unde deleniti incidunt laborum neque! Quod beatae maxime quasi
+culpa ipsam corporis reiciendis deleniti, quis repellendus in nam deserunt, ipsa commodi
+voluptatibus accusamus natus suscipit maiores voluptas odit vitae. Nulla optio, qui alias
+ullam eligendi rem ab molestiae illum, aut dicta reprehenderit consequatur minima
+voluptatibus <Abbr title="[m]essenger [R]ibo-[N]ucleic [A]cid" styles="color:red;" /> quidem
+deleniti sint amet est. Officia natus laudantium quidem animi nihil quae culpa, architecto
+mollitia ipsum. Error sequi illo animi, minima excepturi quasi, enim suscipit eos necessitatibus
+est a saepe cum nihil? Iste, laboriosam, hic neque eius id facilis voluptas beatae non officia
+dolorum quisquam eos quasi optio aut tempora reprehenderit nobis nemo quos cumque ad recusandae
+rem et obcaecati tenetur. Amet ipsam rem architecto, voluptates optio dolores quaerat, dicta
+aliquid doloribus cum iste. Nam illo dolores non placeat perspiciatis, distinctio, eum consequuntur
+earum ullam adipisci officia, architecto totam tempore dolor laudantium. Officia ratione qui
+atque. Aut perferendis molestiae odio sequi explicabo accusamus nesciunt qui debitis aspernatur
+eius illum mollitia vero, distinctio ratione a quod doloremque velit optio porro nulla error
+sed nisi. Assumenda possimus minus repellendus eaque, provident alias, veniam cupiditate, repellat
+sit corporis voluptate quidem unde pariatur totam minima eius facilis! Laboriosam velit quod
+amet, dolorum quae dolore adipisci iusto sint eveniet veritatis unde aliquid recusandae repellat
+impedit atque, iste voluptatem. Sint veniam dolor molestiae aliquam nobis corporis maiores
+id vitae facere doloribus aperiam magnam quo nostrum tempora optio accusamus similique rem
+voluptas, sunt at omnis quis vel, eum nesciunt. Alias eum sapiente inventore obcaecati aliquam
+similique accusantium aspernatur pariatur voluptatum reiciendis officia quam quaerat, vitae
+cum eaque odio asperiores non.
