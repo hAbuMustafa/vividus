16502ff3c995a3eb1e0a65d3dafb75f6b121d71f
hAbuMustafa <hosam5553@gmail.com>
2024-01-25 18:37:36 +0200

    less shipped components

--- svelte/src/lib/index.ts
+++ svelte/src/lib/index.ts
@@ -1,5 +1,5 @@
 // Reexport your entry components here
 import Abbr from '$lib/components/Abbr.svelte';
-import RadialFlickMenu from '$lib/components/RadialFlickMenu.svelte';
+// import RadialFlickMenu from '$lib/components/RadialFlickMenu.svelte';
 
-export { Abbr, RadialFlickMenu };
+export { Abbr /*, RadialFlickMenu*/ };
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import Abbr from '$lib/components/Abbr.svelte';
-  import Table from '$lib/components/Table2.svelte';
+  import Table from '$lib/components/Table.svelte';
 </script>
 
 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint magni, excepturi corrupti
@@ -16,35 +16,19 @@ perferendis tenetur laboriosam ratione doloremque quisquam eos rerum suscipit mi
 voluptatum reiciendis illo, libero voluptatem placeat nam est eaque id quidem dolorem
 possimus. Et rem earum libero a ea eaque officia numquam eum perferendis! Voluptatem
 doloribus consectetur perferendis sit rem, quasi omnis tenetur, maxime suscipit maiores
-<Table
-  data={{
-    caption: { content: 'Table 1: Table caption', side: 'bottom' },
-    thead: [
-      ['c1', { content: 'c2', colspan: 2 }],
-      ['c4', 'c5', 'c6'],
-    ],
-    rows: [
-      [
-        { content: 'hey', scope: 'row' },
-        { content: 'mate', rowspan: 2 },
-      ],
-      [{ content: 'Oh Hi', scope: 'row' }],
-    ],
-    tfoot: [[{ content: 'hello', scope: 'row' }, '', 'Hi']],
-  }}
-></Table>
-minima nesciunt! Distinctio molestiae nostrum nulla totam deserunt animi perferendis doloremque
-facere id laudantium. Earum, aspernatur enim? Dolore illum dolor, repellat architecto veniam,
-velit quis blanditiis facere ipsum similique enim libero eveniet nemo labore? Delectus vel
-quos dolore asperiores sed magni dolores quidem earum ipsa consectetur velit adipisci eveniet
-quo cupiditate, iste eum aut nihil. Maxime accusamus excepturi facilis fugit accusantium maiores
-odit aspernatur, doloribus quo, iure ratione laudantium vel itaque earum quos optio architecto
-nemo dolor perferendis eum ducimus veritatis aut sunt eveniet? Rem, nostrum inventore molestias
-numquam minima quos assumenda ratione iste praesentium? Unde deleniti incidunt laborum neque!
-Quod beatae maxime quasi culpa ipsam corporis reiciendis deleniti, quis repellendus in nam
-deserunt, ipsa commodi voluptatibus accusamus natus suscipit maiores voluptas odit vitae. Nulla
-optio, qui alias ullam eligendi rem ab molestiae illum, aut dicta reprehenderit consequatur
-minima voluptatibus <Abbr
+minima nesciunt! Distinctio molestiae nostrum nulla totam deserunt animi perferendis
+doloremque facere id laudantium. Earum, aspernatur enim? Dolore illum dolor, repellat
+architecto veniam, velit quis blanditiis facere ipsum similique enim libero eveniet nemo
+labore? Delectus vel quos dolore asperiores sed magni dolores quidem earum ipsa
+consectetur velit adipisci eveniet quo cupiditate, iste eum aut nihil. Maxime accusamus
+excepturi facilis fugit accusantium maiores odit aspernatur, doloribus quo, iure ratione
+laudantium vel itaque earum quos optio architecto nemo dolor perferendis eum ducimus
+veritatis aut sunt eveniet? Rem, nostrum inventore molestias numquam minima quos assumenda
+ratione iste praesentium? Unde deleniti incidunt laborum neque! Quod beatae maxime quasi
+culpa ipsam corporis reiciendis deleniti, quis repellendus in nam deserunt, ipsa commodi
+voluptatibus accusamus natus suscipit maiores voluptas odit vitae. Nulla optio, qui alias
+ullam eligendi rem ab molestiae illum, aut dicta reprehenderit consequatur minima
+voluptatibus <Abbr
   title="[m]essenger [R]ibo-[N]ucleic [A]cid"
   --easing="cubic-bezier(.5, 1, .75, 1.25)"
   --duration="0.5s"
