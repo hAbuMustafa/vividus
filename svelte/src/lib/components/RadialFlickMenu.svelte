cbb7168e43028cae541c015f1c71e445b8b42e4f
Hosam Sultan <hosam5553@gmail.com>
2024-04-18 14:05:12 +0000

    trying https://jsbin.com/xudekufavo/edit

--- svelte/src/lib/components/RadialFlickMenu.svelte
+++ svelte/src/lib/components/RadialFlickMenu.svelte
@@ -1,6 +1,109 @@
 <script lang="ts">
   export let options: any[] = [];
 
+  export let size = 120;
+  export let itemThickness = 40;
+  export let showBorder = true;
+  export let borderColor = '#000000';
+
+  const itemPercent = 1 / options.length;
+
+  function polarToCartesian(
+    centerX: number,
+    centerY: number,
+    radius: number,
+    angleInDegrees: number
+  ) {
+    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+    return {
+      x: centerX + radius * Math.cos(angleInRadians),
+      y: centerY + radius * Math.sin(angleInRadians),
+    };
+  }
+
+  function getArc(
+    x: number,
+    y: number,
+    radius: number,
+    startAngle: number,
+    endAngle: number
+  ) {
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+    var d = [
+      'M',
+      start.x,
+      start.y,
+      'A',
+      radius,
+      radius,
+      0,
+      largeArcFlag,
+      0,
+      end.x,
+      end.y,
+    ].join(' ');
+
+    const p = `A ${radius} ${radius} 0 0 0 ${end.x} ${end.y}`;
+
+    return p;
+  }
+</script>
+
+<svg
+  width={size}
+  height={size}
+  viewBox="0 0 {size} {size}"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  style="overflow: visible"
+>
+  {#each options as item, i (i)}
+    <g>
+      <path
+        style="transform-origin: 0 100%; color: black;"
+        stroke={showBorder ? borderColor : null}
+        fill="green"
+        id={`rmi-${i}`}
+        d="
+        M {size / 2} 0
+        {getArc(size / 2, size / 2, size / 2, 0, 360 * itemPercent)}
+        l -{itemThickness} 0
+        M {size / 2} {itemThickness}
+        {getArc(size / 2, size / 2, size / 2 - itemThickness, 0, 360 * itemPercent)}
+        Z
+      "
+      />
+      <text font-size="2" fill="black">
+        <textPath href={`#rmi-${i}`} text-anchor="middle" method="align">
+          {item.title}
+        </textPath>
+      </text>
+    </g>
+  {/each}
+  <circle
+    cx={size / 2}
+    cy={size / 2}
+    r={size / 2 - itemThickness}
+    stroke="yellow"
+    stroke-dasharray="5 3 3 4"
+  />
+  <circle
+    cx={size / 2}
+    cy={size / 2}
+    r={size / 2}
+    stroke="yellow"
+    stroke-dasharray="5 3 3 4"
+  />
+</svg>
+
+<!-- <script lang="ts">
+  export let options: any[] = [];
+
   const itemLength = Math.floor(100 / options.length);
 </script>
 
@@ -39,4 +142,4 @@
       }
     }
   }
-</style>
+</style> -->
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,34 +1,38 @@
 <script>
   import Abbr from '$lib/components/Abbr.svelte';
+  import RadialFlickMenu from '$lib/components/RadialFlickMenu.svelte';
 </script>
 
 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint magni, excepturi corrupti
-modi velit eveniet possimus officia nam voluptate consectetur suscipit fugiat dolores
-placeat rem saepe nesciunt ipsum quia facilis corporis repudiandae nostrum eaque, fuga
-inventore. Reiciendis accusamus similique suscipit eligendi consectetur mollitia tenetur
-earum a, magni cum quae corporis dolorum est recusandae veritatis pariatur illo incidunt
-quo quas voluptate. Hic dicta consectetur perferendis saepe quas vel labore vero
-accusantium, soluta repellendus corrupti provident quisquam ullam praesentium maxime
-maiores deserunt cum dolorum blanditiis adipisci, laborum amet molestiae. Id labore quod
-officia necessitatibus at quisquam voluptate inventore optio tempore a. Velit aspernatur
-perferendis tenetur laboriosam ratione doloremque quisquam eos rerum suscipit minima
-voluptatum reiciendis illo, libero voluptatem placeat nam est eaque id quidem dolorem
-possimus. Et rem earum libero a ea eaque officia numquam eum perferendis! Voluptatem
-doloribus consectetur perferendis sit rem, quasi omnis tenetur, maxime suscipit maiores
-minima nesciunt! Distinctio molestiae nostrum nulla totam deserunt animi perferendis
-doloremque facere id laudantium. Earum, aspernatur enim? Dolore illum dolor, repellat
-architecto veniam, velit quis blanditiis facere ipsum similique enim libero eveniet nemo
-labore? Delectus vel quos dolore asperiores sed magni dolores quidem earum ipsa
-consectetur velit adipisci eveniet quo cupiditate, iste eum aut nihil. Maxime accusamus
-excepturi facilis fugit accusantium maiores odit aspernatur, doloribus quo, iure ratione
-laudantium vel itaque earum quos optio architecto nemo dolor perferendis eum ducimus
-veritatis aut sunt eveniet? Rem, nostrum inventore molestias numquam minima quos assumenda
-ratione iste praesentium? Unde deleniti incidunt laborum neque! Quod beatae maxime quasi
-culpa ipsam corporis reiciendis deleniti, quis repellendus in nam deserunt, ipsa commodi
-voluptatibus accusamus natus suscipit maiores voluptas odit vitae. Nulla optio, qui alias
-ullam eligendi rem ab molestiae illum, aut dicta reprehenderit consequatur minima
-voluptatibus <Abbr title="[m]essenger [R]ibo-[N]ucleic [A]cid" styles="color:red;" /> quidem
-deleniti sint amet est. Officia natus laudantium quidem animi nihil quae culpa, architecto
+modi velit eveniet possimus officia nam voluptate consectetur suscipit fugiat dolores <RadialFlickMenu
+  showBorder={false}
+  options={[{ title: 'home' }, { title: 'about' }, { title: 'login' }]}
+/>
+placeat rem saepe nesciunt ipsum quia facilis corporis repudiandae nostrum eaque, fuga inventore.
+Reiciendis accusamus similique suscipit eligendi consectetur mollitia tenetur earum a, magni
+cum quae corporis dolorum est recusandae veritatis pariatur illo incidunt quo quas voluptate.
+Hic dicta consectetur perferendis saepe quas vel labore vero accusantium, soluta repellendus
+corrupti provident quisquam ullam praesentium maxime maiores deserunt cum dolorum blanditiis
+adipisci, laborum amet molestiae. Id labore quod officia necessitatibus at quisquam voluptate
+inventore optio tempore a. Velit aspernatur perferendis tenetur laboriosam ratione doloremque
+quisquam eos rerum suscipit minima voluptatum reiciendis illo, libero voluptatem placeat nam
+est eaque id quidem dolorem possimus. Et rem earum libero a ea eaque officia numquam eum perferendis!
+Voluptatem doloribus consectetur perferendis sit rem, quasi omnis tenetur, maxime suscipit
+maiores minima nesciunt! Distinctio molestiae nostrum nulla totam deserunt animi perferendis
+doloremque facere id laudantium. Earum, aspernatur enim? Dolore illum dolor, repellat architecto
+veniam, velit quis blanditiis facere ipsum similique enim libero eveniet nemo labore? Delectus
+vel quos dolore asperiores sed magni dolores quidem earum ipsa consectetur velit adipisci eveniet
+quo cupiditate, iste eum aut nihil. Maxime accusamus excepturi facilis fugit accusantium maiores
+odit aspernatur, doloribus quo, iure ratione laudantium vel itaque earum quos optio architecto
+nemo dolor perferendis eum ducimus veritatis aut sunt eveniet? Rem, nostrum inventore molestias
+numquam minima quos assumenda ratione iste praesentium? Unde deleniti incidunt laborum neque!
+Quod beatae maxime quasi culpa ipsam corporis reiciendis deleniti, quis repellendus in nam
+deserunt, ipsa commodi voluptatibus accusamus natus suscipit maiores voluptas odit vitae. Nulla
+optio, qui alias ullam eligendi rem ab molestiae illum, aut dicta reprehenderit consequatur
+minima voluptatibus <Abbr
+  title="[m]essenger [R]ibo-[N]ucleic [A]cid"
+  styles="color:red;"
+/> quidem deleniti sint amet est. Officia natus laudantium quidem animi nihil quae culpa, architecto
 mollitia ipsum. Error sequi illo animi, minima excepturi quasi, enim suscipit eos necessitatibus
 est a saepe cum nihil? Iste, laboriosam, hic neque eius id facilis voluptas beatae non officia
 dolorum quisquam eos quasi optio aut tempora reprehenderit nobis nemo quos cumque ad recusandae
