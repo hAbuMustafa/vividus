a9232f9363e3637c691b9bc301b981bd54e076e6
hAbuMustafa <hosam5553@gmail.com>
2025-01-08 02:12:20 +0200

    types for new Ayah Component

--- svelte/src/app.d.ts
+++ svelte/src/app.d.ts
@@ -1,12 +1,46 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
+
+  namespace Quran {
+    interface Ayah {
+      id: number;
+      number: number;
+      text: string;
+      translations: Translations;
+      transliteration: string;
+      chapter: Chapter;
+    }
+
+    interface Chapter {
+      id: number;
+      name: string;
+      transliteration: string;
+      translations?: Translations;
+      type: string;
+      total_verses?: number;
+      verses?: Ayah[];
+      link?: string;
+    }
+
+    interface Translations {
+      en: string;
+      es: string;
+      fr: string;
+      id: string;
+      ru: string;
+      sv: string;
+      tr: string;
+      ur: string;
+      zh: string;
+    }
+  }
 }
 
 export {};
