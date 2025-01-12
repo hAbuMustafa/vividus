cabacadc3361274b7300d178ca218320abee40aa
hAbuMustafa <hosam5553@gmail.com>
2025-01-13 01:57:43 +0200

    used new wrapper

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Abbr from '$lib/components/Abbr.svelte';
   import Ayah from '$lib/components/Ayah.svelte';
+  import Component from '$lib/components/docs/ComponentWrapper.svelte';
   import RadialFlickMenu from '$lib/components/RadialFlickMenu.svelte';
   import Sparkles from '$lib/components/Sparkles.svelte';
 </script>
@@ -12,38 +13,96 @@
 </h2>
 
 <div class="component-list">
-  <div class="component-item">
-    <Sparkles>Whimsical</Sparkles>
-  </div>
+  <Component name="Sparkles" usage="<Sparkles>Whimsical</Sparkles>">
+    {#snippet Result()}
+      <Sparkles>Whimsical</Sparkles>
+    {/snippet}
+  </Component>
 
-  <div class="component-item">
-    <RadialFlickMenu
+  <Component
+    name="RadialFlickMenu"
+    usage={`<RadialFlickMenu 
       options={[
-        {
-          title: 'home',
-          action: () => {
-            alert('home');
+        {title:'home', action:()=>{alert('home')}},
+        {title:'about', action:()=>console.log('about')},
+        {title:'login', action:()=>alert('login')}
+        ]}
+        />`}
+  >
+    {#snippet Result()}
+      <RadialFlickMenu
+        options={[
+          {
+            title: 'home',
+            action: () => {
+              alert('home');
+            },
           },
-        },
-        { title: 'about', action: () => console.log('about') },
-        { title: 'login', action: () => alert('login') },
-        {
-          action: () => {
-            console.log('hi');
-          },
-          title: 'hi',
-        },
-      ]}
-    />
-  </div>
+          { title: 'about', action: () => console.log('about') },
+          { title: 'login', action: () => alert('login') },
+        ]}
+      />
+    {/snippet}
+    {#snippet Attrs()}
+      <li class="optional">options: MenuItem[]</li>
+      <li class="optional">size: number</li>
+      <li class="optional">itemThickness: number</li>
+      <li class="optional">showBorder: boolean</li>
+      <li class="optional">borderColor: string</li>
+      <li class="optional">menuItemColor: string</li>
+      <li class="optional">dismissButtonColor: string</li>
+    {/snippet}
+  </Component>
 
-  <div class="component-item">
-    <Abbr title="[m]essenger [R]ibo-[N]ucleic [A]cid" styles="color:red;" />
-  </div>
+  <Component
+    name="Abbr"
+    usage={`<Abbr title="[m]essenger [R]ibo-[N]ucleic [A]cid" styles="color:red;" />`}
+  >
+    {#snippet Result()}
+      <Abbr title="[m]essenger [R]ibo-[N]ucleic [A]cid" styles="color:red;" />
+    {/snippet}
+    {#snippet Attrs()}
+      <li>title: string</li>
+      <li class="optional">styles: string</li>
+      <li class="optional">partStyles: string</li>
+      <li class="optional">segmentStyles: string</li>
+      <li class="optional">useTitle: boolean</li>
+      <li class="optional">useTransition: boolean</li>
+    {/snippet}
+  </Component>
 
-  <div class="component-item" dir="rtl">
-    <Ayah number="112:2-114:2" showLink={true} />
-  </div>
+  <Component
+    name="Ayah"
+    usage={`<Ayah number="11:2" showLink={true} />
+  
+<Ayah number="11:2" showReference={false} />
+
+<Ayah number="11:2" showNumbers={false} />
+
+<Ayah number="112" />
+
+<Ayah number="112:2-114:2" />
+`}
+  >
+    {#snippet Result()}
+      <Ayah number="11:2" showLink={true} />
+      <hr />
+      <Ayah number="11:2" showReference={false} />
+      <hr />
+      <Ayah number="11:2" showNumbers={false} />
+      <hr />
+      <Ayah number="112" />
+      <hr />
+      <Ayah number="112:2-114:2" />
+    {/snippet}
+    {#snippet Attrs()}
+      <li>number: string</li>
+      <li class="optional">styles: string</li>
+      <li class="optional">showNumbers: boolean</li>
+      <li class="optional">showReference: boolean</li>
+      <li class="optional">showLink: boolean</li>
+    {/snippet}
+  </Component>
 </div>
 
 <style>
@@ -62,18 +121,8 @@
     align-items: center;
   }
 
-  .component-item {
-    padding: 1rem;
-    width: 55%;
-    border: 1px solid #ccc;
-    border-radius: 0.5rem;
-    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
-    transition: all 0.2s ease-in-out;
-
-    display: grid;
-  }
-
-  .component-item > :global(*) {
-    place-self: center;
+  hr {
+    border: 1px solid black;
+    width: 100%;
   }
 </style>
